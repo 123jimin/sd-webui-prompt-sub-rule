a58fe9d25c4f378801380783e7bcbb330b63175a
Jimin Park <pjm950@gmail.com>
2023-07-01 15:11:54 +0900

    Added re: and keyword:

--- replacer.py
+++ replacer.py
@@ -1,24 +1,39 @@
 from prompt import Prompt, Token, TokenKind
+import re
 
 class Entry:
     name: str
     out: Prompt
-    tags: set
+    tags: set[str]
+    keywords: list[str]
+    tag_regexs: list[re.Pattern]
 
     def __init__(self, name: str):
         self.name = name
         self.out = None
         self.tags = set()
+        self.tag_regexs = []
+
+    def add_extra_networks(self, prompt: Prompt):
+        for extra in self.out.extra_networks:
+            prompt.add_extra_network(extra)
 
     def process(self, prompt: Prompt):
         for i in range(len(prompt.tokens)):
             token = prompt.tokens[i]
             if token.kind != TokenKind.TEXT: continue
-            if str(token).lower() in self.tags:
+
+            token_text = str(token)
+            
+            if token_text.lower() in self.tags:
                 prompt.tokens = prompt.tokens[:i] + self.out.tokens + prompt.tokens[i+1:]
-                for extra in self.out.extra_networks:
-                    prompt.add_extra_network(extra)
+                self.add_extra_networks(prompt)
                 return
+        
+            for tag_regex in self.tag_regexs:
+                if tag_regex.search(token_text):
+                    self.add_extra_networks(prompt)
+                    return
 
 class Replacer:
     entries: list[Entry]
@@ -45,11 +60,18 @@ class Replacer:
 
             if line.startswith("for:"):
                 curr_entry.out = Prompt(line[4:].lstrip())
+
             if line.startswith("tag:"):
                 for token in Prompt(line[4:].lstrip()).tokens:
                     if token.kind == TokenKind.TEXT:
                         curr_entry.tags.add(str(token).lower())
-                
+            
+            if line.startswith("re:"):
+                curr_entry.tag_regexs.append(re.compile(line[3:].lstrip(), re.I))
+            
+            if line.startswith("keyword:"):
+                keyword_regex = "|".join(keyword.strip() for keyword in line[8:].split(","))
+                curr_entry.tag_regexs.append(re.compile(keyword_regex, re.I))
 
     def replace(self, prompt_txt: str):
         if not len(self.entries): return prompt_txt
